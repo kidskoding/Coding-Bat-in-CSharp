2494ffc1748504adc36385f790ef1f9aae3d6d1b
Anirudh Konidala <kidskodingclub@gmail.com>
2023-03-03 19:56:33 -0600

    added problems 1 and 2 from coding bat warmup-1

---
+++ Warmup-1/MainClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmup_1
+{
+    internal class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
---
+++ Warmup-1/Warmup_1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codingbat
+{
+    internal class Warmup_1
+    {
+        public static bool sleepIn(bool weekday, bool vacation)
+        {
+            return !weekday || vacation;
+        }
+        public static bool monkeyTrouble(bool aSmile, bool bSmile)
+        {
+            return aSmile && bSmile || !aSmile && !bSmile;
+        }
+    }
+}
